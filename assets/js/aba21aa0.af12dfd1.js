b22d1b76197369b9c9ff2cbf9a8cad0a9c8c69b9
Ольга <olga@MacBook-Air-Olga.local>
2024-12-06 02:51:03 +0300

    Deploy website - based on 7efbf58df4695217a3b037d53ef80f5f21747b41

---
+++ assets/js/aba21aa0.af12dfd1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/API/cosmeticsapp.md
+++
@@ -1,8 +0,0 @@
----
-title: API 1 - Swagger CosmeticsApp
-hide_table_of_contents: true
----
-
-import ApiDocMdx from '@theme/ApiDocMdx';
-
-<ApiDocMdx id="cosmeticsapp" />
